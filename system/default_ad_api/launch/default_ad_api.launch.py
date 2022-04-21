56ca85cfc2fdd780d5bb4066bad14a52cfd03318
Takagi, Isamu <isamu.takagi@tier4.jp>
2022-04-22 04:52:06 +0900

    WIP

--- system/default_ad_api/launch/default_ad_api.launch.py
+++ system/default_ad_api/launch/default_ad_api.launch.py
@@ -31,6 +31,7 @@ def _create_api_node(node_name, class_name, **kwargs):
 def generate_launch_description():
     components = [
         _create_api_node("interface_version", "InterfaceVersionNode"),
+        _create_api_node("driving", "DrivingNode"),
     ]
     container = ComposableNodeContainer(
         namespace="default_ad_api",
