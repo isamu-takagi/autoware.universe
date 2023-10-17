db11e2037ae33a4fef68404ae13d054642539ed7
Takagi, Isamu <isamu.takagi@tier4.jp>
2023-10-17 21:31:57 +0900

    add sample code
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

---
+++ system/system_diagnostic_viewer/python/plugin.py
@@ -0,0 +1,29 @@
+# Copyright 2023 The Autoware Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from rqt_gui_py.plugin import Plugin
+
+
+class SystemDiagnosticMonitorPlugin(Plugin):
+    def __init__(self, context):
+        super().__init__(context)
+
+    def shutdown_plugin(self):
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        pass
