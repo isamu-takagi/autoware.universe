574f62c06c866c3260a687469f9e9d7cbe544c53
Takagi, Isamu <isamu.takagi@tier4.jp>
2024-03-20 18:14:50 +0900

    feat: add types
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- system/diagnostic_graph_aggregator/src/common/graph/types.hpp
+++ system/diagnostic_graph_aggregator/src/common/graph/types.hpp
@@ -17,6 +17,14 @@
 
 #include <diagnostic_msgs/msg/diagnostic_array.hpp>
 #include <diagnostic_msgs/msg/diagnostic_status.hpp>
+#include <tier4_system_msgs/msg/diag_graph_status.hpp>
+#include <tier4_system_msgs/msg/diag_graph_struct.hpp>
+#include <tier4_system_msgs/msg/diag_leaf_status.hpp>
+#include <tier4_system_msgs/msg/diag_leaf_struct.hpp>
+#include <tier4_system_msgs/msg/diag_link_status.hpp>
+#include <tier4_system_msgs/msg/diag_link_struct.hpp>
+#include <tier4_system_msgs/msg/diag_node_status.hpp>
+#include <tier4_system_msgs/msg/diag_node_struct.hpp>
 #include <tier4_system_msgs/msg/diagnostic_graph.hpp>
 #include <tier4_system_msgs/msg/diagnostic_link.hpp>
 #include <tier4_system_msgs/msg/diagnostic_node.hpp>
@@ -26,6 +34,14 @@ namespace diagnostic_graph_aggregator
 
 using diagnostic_msgs::msg::DiagnosticArray;
 using diagnostic_msgs::msg::DiagnosticStatus;
+using tier4_system_msgs::msg::DiagGraphStatus;
+using tier4_system_msgs::msg::DiagGraphStruct;
+using tier4_system_msgs::msg::DiagLeafStatus;
+using tier4_system_msgs::msg::DiagLeafStruct;
+using tier4_system_msgs::msg::DiagLinkStatus;
+using tier4_system_msgs::msg::DiagLinkStruct;
+using tier4_system_msgs::msg::DiagNodeStatus;
+using tier4_system_msgs::msg::DiagNodeStruct;
 using tier4_system_msgs::msg::DiagnosticGraph;
 using tier4_system_msgs::msg::DiagnosticLink;
 using tier4_system_msgs::msg::DiagnosticNode;
